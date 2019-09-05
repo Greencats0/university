dca712c1982365ffb16b1083cf048b5b8e63bee6
JaredDyreson <jared.dyreson@gmail.com>
2019-09-05 09:53:52 -0700

    Added lab two for CPSC-223J

---
+++ 3/Fall/CPSC-223J/assignments/01/assets/combine.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+convert Selection_184.png cpp_to_java_cli_out.png Selection_185.png mailing_address_cli_out.png Selection_186.png name_age_cli_out.png ../lab_one.pdf
+
--- 3/Fall/CPSC-223J/assignments/01/src/CPPToJava.java
+++ 3/Fall/CPSC-223J/assignments/01/src/CPPToJava.java
@@ -9,22 +9,32 @@ CWID: 889546529
 import java.util.Scanner;
 public class CPPToJava{
 	public static void main(String args[]){
-
-		// instantiate standard input
 		Scanner stdin = new Scanner(System.in);
-		// prompt for user to put their information into
-		System.out.print("Enter a year: ");
-		// capture user input
+		System.out.print("Enter year: ");
 		int year = stdin.nextInt();
-
-		// since the conditionals provided ask if the year is fully divisible by 4, 100, 400, we can just make this all one initial conditional statement
-		if((year % 4 == 0) || (year % 100 == 0) || (year % 400 == 0)){
-			System.out.println(year + " is a leap year");	
+		// if the year divisible by four
+		if(year % 4 == 0){
+			// if the year is divisble fully by 100
+			if(year % 100 == 0){
+				// if the year is fully divisible by 400
+				if(year % 400 == 0){
+					// the year is a leap year
+					System.out.println(year+ " is a leap year");
+				}
+				else{
+					// not divisble fully by 400 means it is not a leap year
+					System.out.println(year + " is not a leap year");
+				}
+			}
+			else{
+				// this print statement is redundant because 100 and 400 share a common factor of 100
+				System.out.println(year + " is a leap year");
+			}
+		
 		}
 		else{
-			// print if condition fails
+			// if the number is not even divisible by 4, we discard it
 			System.out.println(year + " is not a leap year");
-
 		}
 	}
 }
