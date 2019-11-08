f6e8cc460ab18f472c166c34867691624efbacbf
JaredDyreson <jared.dyreson@gmail.com>
2019-11-08 13:56:47 -0800

    Automatic backup from the terminal...beep bloop

--- 3/Fall/CPSC-223J/notes/keyboard/Cursor.java
+++ 3/Fall/CPSC-223J/notes/keyboard/Cursor.java
@@ -3,23 +3,30 @@
 Jared Dyreson
 CWID: 889546529
 Cursor.java -> 
+csrc_compile: TRUE
 
 */
 
 import java.awt.*;
 
 public class Cursor {
+        // positions in R^2
 	private int X = 0, Y = 0;
+
+        // our what our cursor looks like and what kind of paint color it uses
         private Color cursor_color = Color.YELLOW;
         private Color paint_color = Color.WHITE;
+        // painting flag
         private boolean space_is_marked = false;
 
+        // getters
         public int get_x(){ return this.X; }
         public int get_y(){ return this.Y; }
         public Color get_color(){ return this.cursor_color; }
         public Color get_paint_color(){ return this.paint_color; }
         public boolean get_space_flag(){ return space_is_marked; }
 
+        // setters
         public void set_position(int x, int y){
                 this.X = x;
                 this.Y = y;
@@ -36,7 +43,7 @@ public class Cursor {
         public void toggle_marker(boolean value){
                 this.space_is_marked = value;
         }
-
+        // our constructor is a glorified setter
         public Cursor(int x, int y){
                 this.set_position(x, y);
         }
--- 3/Fall/CPSC-223J/notes/keyboard/JDemoKey.java
+++ 3/Fall/CPSC-223J/notes/keyboard/JDemoKey.java
@@ -3,7 +3,7 @@
 Jared Dyreson
 CWID: 889546529
 JDemoKey.java -> 
-csrc_compile: FALSE
+csrc_compile: TRUE
 
 */
 
@@ -12,21 +12,30 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class JDemoKey extends JFrame implements KeyListener{
+
+        // the boundaries for the checker board
         private final int FRAME_HEIGHT = 500, FRAME_WIDTH = 500, ROWS = 16, COLS = 16;
         
+        // we need two parallel arrays to keep track of the colors on the board
+        // this one keeps track of the colors for the pane
         private JPanel[][] tpanel = new JPanel[ROWS][COLS];
+        // this keeps track of what colors the tpanel object reflects
         private Color[][] color_panel = new Color[ROWS][COLS];
+        // the main container for the checker board layout
         private JPanel pane = new JPanel(new GridLayout(ROWS, COLS,2, 2));
 
         private Color w = Color.WHITE;
         private Color b = Color.BLACK;
-        private Color previous_color, current_color, next_color;
+        // so we can keep track of what colors are around us
+        private Color previous_color, current_color;
 
         private Cursor cursor = new Cursor(0, 0);
 
+        // setters
         public void set_previous_color(Color c){ this.previous_color = c; }
         public void set_current_color(Color c){ this.current_color = c; }
 
+        // getters
         public Color get_previous_color(){ return this.previous_color; }
         public Color get_current_color(){ return this.current_color; }
 
@@ -39,6 +48,7 @@ public class JDemoKey extends JFrame implements KeyListener{
 
                 for(int x = 0; x < COLS; ++x){
                         for(int y = 0; y < ROWS; ++y){
+                                // make the grid and corresponding parallel array with all white tiles
                                 tpanel[x][y] = new JPanel();
                                 color_panel[x][y] = this.w;
                                 this.pane.add(tpanel[x][y]);
@@ -49,53 +59,54 @@ public class JDemoKey extends JFrame implements KeyListener{
                 this.add(pane, BorderLayout.CENTER);
 
                 addKeyListener(this);
+                // initialize the cursor
                 this.tpanel[0][0].setBackground(this.cursor.get_color());
         }
 
         public void set_position(int x, int y, boolean coloring) throws IndexOutOfBoundsException{
-
+                
+                // initial position
                 int x_naught = this.cursor.get_x();
                 int y_naught = this.cursor.get_y();
 
+                // move the cursor over
                 this.tpanel[y][x].setBackground(this.cursor.get_color());
+                // update the current and previous colors
                 this.set_previous_color(this.color_panel[y_naught][x_naught]);
                 this.set_current_color(this.color_panel[y][x]);
 
-
+                // we pass in a boolean flag to allow us to use the same function
+                // with different conditions
+                
                 if(coloring){
-                        if(this.current_color == Color.BLACK){
+                        // erase black tiles
+                        if(this.previous_color == Color.BLACK){
                                 this.color_panel[y_naught][x_naught] = this.w;
                                 this.tpanel[y_naught][x_naught].setBackground(this.w);
                                 this.previous_color = Color.WHITE;
                         }
+                        // mark tiles
                         else if(this.current_color == Color.WHITE){
                                 this.color_panel[y_naught][x_naught] = this.b;
                                 this.tpanel[y_naught][x_naught].setBackground(this.b);
                                 this.previous_color = Color.BLACK;
                         }
                 }
+                // just continue as normal
                 else{
                         this.tpanel[y_naught][x_naught].setBackground(this.previous_color);
-                        //if(previous_color == Color.WHITE){
-                                //System.out.println("this");
-                                //this.tpanel[y_naught][x_naught].setBackground(this.w);
-                        //}
-                        //else if(previous_color == Color.BLACK){
-                                //System.out.println("that");
-                        //}
                 }
-
-
-                this.print_color_name(this.previous_color);
+                // update cursor location
                 this.cursor.set_position(x, y);
+                // make the pane reload
                 this.pane.revalidate();
                 this.pane.repaint();
+                // we are only allowing for one pixel being marked
+                // at a time
                 this.cursor.toggle_marker(false);
 
         }
-        public void print_color_name(Color c){
-                System.out.println(c.toString());
-        }
+
         @Override
         public void keyTyped(KeyEvent event){
                 // only executes when char is typed
@@ -104,15 +115,22 @@ public class JDemoKey extends JFrame implements KeyListener{
 
         @Override
         public void keyPressed(KeyEvent event){
+
                 int key_code = event.getKeyCode();
 
+                // get the position of the cursor
                 int x = this.cursor.get_x();
                 int y = this.cursor.get_y();
+                // we have a try catch block to basically ignore the fact that we bump against a wall
+                // the catch does nothing
                 try{
-
+                        // if the space key is pressed, we can indicate we want to draw
                         if(key_code == KeyEvent.VK_SPACE){
                                 this.cursor.toggle_marker(true);
                         }
+                        // -/+ are flipped because of the frame of reference
+                        // - means we want to go up/right because the indexes start from 0
+                        // + means we want to go down/left because the indexes start from 0
 
                         if(key_code == KeyEvent.VK_UP){
                                 this.set_position(x, y-1, this.cursor.get_space_flag());
